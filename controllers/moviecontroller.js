4cd0eaa5c9e3a84fbd14efb2af72ecf81c50e20c
RosaCasa <rosamari.sab@gmail.com>
2020-11-03 18:00:33 +0100

    finished movie controller

--- controllers/moviecontroller.js
+++ controllers/moviecontroller.js
@@ -82,6 +82,36 @@ const searchByNewest = async (req, res) => {
 };
 
 
+const searchByOldest = async (req, res) => {
+
+    try {
+        let query = req.query;
+        let limit = parseInt(query.limit);
+
+        if (!limit || limit >= 30) {
+            limit = 30
+        };
+
+        // busco todas las películas que sean less than or equal al día de hoy
+        // luego las ordeno por release_date con .sort
+        let movies = await MovieModel.find({
+            release_date:
+                { $lte: new Date() }
+        })
+            .sort({
+                release_date: 1
+            })
+            .limit(limit);
+
+        res.send(movies)
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({ message: 'No se ha podido mostrar las películas más recientes.' });
+    }
+};
+
+
 const showUpcoming = async (req, res) => {
 
     try {
@@ -123,9 +153,9 @@ const searchByTitle = async (req, res) => {
 
         let title = new RegExp(req.query.title, 'i');
 
-        let movies = await MovieModel.find({title})
-        .limit(limit)
-    
+        let movies = await MovieModel.find({ title })
+            .limit(limit)
+
         res.send(movies)
 
     } catch (error) {
@@ -135,43 +165,57 @@ const searchByTitle = async (req, res) => {
 };
 
 
-// const searchById = async (req, res) => {
+const searchById = async (req, res) => {
 
-//     try {
-//         let query = req.query;
-//         let limit = parseInt(query.limit);
+    try {
+        let query = req.query;
+        let limit = parseInt(query.limit);
 
-//         if (!limit || limit >= 30) {
-//             limit = 30
-//         };
+        if (!limit || limit >= 30) {
+            limit = 30
+        };
 
-//         let id = new RegExp(req.query.id);
+        let id = req.query.id;
 
-//         let movies = await MovieModel.find({id})
-//         .limit(limit)
-    
-//         res.send(movies)
+        let movies = await MovieModel.find({ id })
+            .limit(limit)
 
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).send({ message: 'No se encontró nada.' });
-//     }
-// };
+        res.send(movies)
 
-
-// const searchByGenre = async (req, res) => {
-
-//     try {
-
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).send({ message: 'No se encontró nada.' });
-//     }
-// };
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({ message: 'No se encontró nada.' });
+    }
+};
 
 
+const searchByGenre = async (req, res) => {
+
+    try {
+        let query = req.query;
+        let limit = parseInt(query.limit);
+        let idGenre = parseInt(req.query.genre);
+
+        if (!limit || limit >= 30) {
+            limit = 30
+        };
+
+
+        let movies = await MovieModel.find({
+            genre_ids: idGenre
+        }).limit(limit)
+
+        res.send(movies);
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({ message: 'No se encontró nada.' });
+    }
+};
 
 
 
 
-module.exports = { showMovies, searchByPopularity, searchByNewest, showUpcoming, searchByTitle};
+
+
+module.exports = { showMovies, searchByPopularity, searchByNewest, searchByOldest, showUpcoming, searchByTitle, searchById, searchByGenre };
