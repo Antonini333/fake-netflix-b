6e460936fa99dc0370f6884b4c5ec2593c8b50de
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-03 13:30:32 +0100

    Finished Register, Login and Logout

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -2,10 +2,14 @@
 const router = require ("express").Router();
 const UserController = require("../controllers/usercontroller");
 
-router.post('/user/register', UserController.Register);
-router.post('/user/login', UserController.Login);
-router.post('/user/logout', UserController.Logout);
-router.delete('/user/delete', UserController.Delete);
-router.get('/user/Profile', UserController.Profile);
+router.get('/users', UserController.getAll)
+
+router.post('/user/register', UserController.Register); // CHECkED
+router.post('/user/login', UserController.Login); //CHECKED
+router.post('/user/logout', UserController.Logout); // CHECKED
+
+router.delete('/user/delete/:id', UserController.Delete); // En vez de eliminar, cambia los valores a null
+
+//router.get('/user/Profile', UserController.Profile);
 
 module.exports = router;
