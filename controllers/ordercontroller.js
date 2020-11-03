a1e3be27dd919e501648a38203f1b07e99b66de1
javigarcia <javigarciaweb@gmail.com>
2020-11-03 14:01:03 +0100

    Implemented rent endpoint

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -4,6 +4,27 @@ const UserModel = require('../models/User');
 
 
 const OrderController = {
+    async rent (req,res) {
+        let user = await UserModel.findOne({
+           _id: req.body._id
+        })
+        let movie = await MovieModel.findOne({
+            id: req.body.id
+        });
+        try {
+            const order = await OrderModel({
+                userId: user,
+                movieId: movie,
+            }).save();
+                res.status(201).send(order);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            error,
+            message: 'There was a problem trying to register the order.'
+        })
+    }
+    }
     
 
 
--- routes/orderrouter.js
+++ routes/orderrouter.js
@@ -1,11 +1,11 @@
-const router = require ("express").Router();
+const router = require("express").Router();
 const OrderController = require('../controllers/ordercontroller');
 
 
-router.get('/order/show', showAll);
-router.get('/order/show/:userId', showUser);
 router.post('/order', rent);
-router.delete('/order/:orderId',cancelOrder);
+//router.get('/order/show', showAll);
+//router.get('/order/show/:userId', showUser);
+//router.delete('/order/:orderId', cancelOrder);
 
 
 module.exports = router;
