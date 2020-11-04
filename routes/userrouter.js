cd15d9699a1d2a50d48c71f4783c5d653fdaf50a
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-04 12:26:59 +0100

    ready to merge

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -1,21 +1,24 @@
 const router = require ("express").Router();
 const UserController = require("../controllers/usercontroller");
 
-///MIDDLEWARE
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
-const checkadult = require ("../middleware/checkadult");
+
+//              --MIDDLEWARE--
+const auth = require("../middleware/auth"); //Checked
+const admin = require("../middleware/admin"); // Checked
+const checkadult = require ("../middleware/checkadult"); // Checked
+
+
+//                --RUTAS--
+router.get('/users', UserController.GetAll) // Checked
+
+router.post('/user/register', UserController.Register); // Checked
+router.post('/user/login', UserController.Login); //Checked
+router.post('/user/logout', auth, UserController.Logout); // Checked
+
+router.delete('/user/delete/:id', auth, admin, UserController.Delete); // Checked
 
 
 
-router.get('/users', UserController.getAll) //CHECKED
-
-router.post('/user/register', UserController.Register); // CHECkED
-router.post('/user/login', UserController.Login); //CHECKED
-router.post('/user/logout', auth, UserController.Logout); // CHECKED
-
-router.delete('/user/delete/:id', auth, admin, UserController.Delete); // CHECKED
-
-//router.get('/user/Profile', UserController.Profile);
+//router.get('/user/Profile', UserController.Profile);  ¿ES NECESARIA?
 
 module.exports = router;
