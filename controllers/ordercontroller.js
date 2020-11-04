d3381027b87946107241250ba9d8bcffadcbdfe5
javigarcia <javigarciaweb@gmail.com>
2020-11-04 13:40:28 +0100

    Add extend rental endpoint and dayjs library

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -1,19 +1,18 @@
 const OrderModel = require('../models/Order');
 const MovieModel = require('../models/Movie');
 const UserModel = require('../models/User');
-
+const dayjs = require ("dayjs");
 
 
 const OrderController = {
-    async rent (req,res) {
+    async rent(req, res) {
         let user = await UserModel.findOne({
-           email: req.body.email
+            email: req.body.email
         })
         let movie = await MovieModel.findOne({
             id: req.body.movieId
         })
-        let date = Date.now()
-        let endDate = date + 172800000
+        let endDate = dayjs().add(2, 'day')
         try {
             const order = await OrderModel({
                 userId: user._id,
@@ -21,59 +20,86 @@ const OrderController = {
                 rentalDate: Date.now(),
                 rentalEndDate: endDate
             }).save();
-                res.status(201).send(order);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({
-            error,
-            message: 'There was a problem trying to register the order.'
-        })
-    }
+            res.status(201).send(order);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                error,
+                message: 'There was a problem trying to register the order.'
+            })
+        }
     },
-    async showAll (req, res){
+    async showAll(req, res) {
         try {
             const orders = await OrderModel.find()
             res.status(200).send(orders)
         } catch (error) {
             console.error(error);
             res.status(500).send({
-            error,
-            message: 'There was a problem trying to retrive the orders.'
-        })
+                error,
+                message: 'There was a problem trying to retrive the orders.'
+            })
         }
     },
-    async showUser (req,res){
+    async showUser(req, res) {
         try {
             const userOrders = await OrderModel.find({
                 userId: req.params.userId
             });
-            res.status(201).send({userOrders});
+            res.status(201).send({
+                userOrders
+            });
         } catch (error) {
             console.error(error);
             res.status(500).send({
-            error,
-            message: 'There was a problem trying to retrive the orders.'
-        })
+                error,
+                message: 'There was a problem trying to retrive the orders.'
+            })
         }
     },
-    async cancelOrder (req, res){
+    async cancelOrder(req, res) {
         try {
             await OrderModel.findByIdAndDelete({
-                _id:req.params._id
+                _id: req.params._id
             });
             res.status(201).send({
                 message: `El alquiler ha sido cancelado exitosamente`
-            });         
+            });
         } catch (error) {
             console.error(error);
             res.status(500).send({
-            error,
-            message: 'There was a problem trying cancel order.'
-        })
+                error,
+                message: 'There was a problem trying cancel order.'
+            })
+        }
+    },
+    async extendOrder(req, res) {
+        const order = await OrderModel.findById(req.body._id);
+        const newRentalEnd = dayjs(order.rentalEndDate).add(req.body.days, 'day');
+        
+        
+
+        try {
+            await OrderModel.findOneAndUpdate({
+                _id: req.body._id
+            }, 
+               {rentalEndDate: newRentalEnd} 
+            , {
+                new: true,
+                useFindAndModify: false
+            });
+            res.status(200).send({
+                message: 'Extend your order is Ok'
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                error,
+                message: 'There was a problem trying to extend your order'
+            })
+
         }
     }
-    
-
 
 
 }
--- routes/orderrouter.js
+++ routes/orderrouter.js
@@ -6,6 +6,7 @@ router.post('/order/rent', OrderController.rent);
 router.get('/order/show', OrderController.showAll);
 router.get('/order/show/:userId', OrderController.showUser);
 router.delete('/order/:orderId', OrderController.cancelOrder);
+router.put('/order/', OrderController.extendOrder);
 
 
 module.exports = router;
