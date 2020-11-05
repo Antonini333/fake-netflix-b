fc59002287e303f96266e72a14725a62e1487e6f
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-05 10:36:03 +0100

    Switched to Bearer Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,18 @@ const UserModel = require('../models/User');
 
 const auth = async(req,res,next) => {
     try{
-      const token = req.headers.authorization;
+      const token = req.header('Authorization').replace('Bearer ', '');
       jwt.verify(token,'mymotherpetsme');
       const user = await UserModel.findOne({token:token});
       console.log(user)
       if(!user){
-        return res.status(401).send({message: 'You are not authorized.'})
+        return res.status(401).send({message: 'You got no token.'})
       }
       req.user = user;
       next();
     } catch (error){
       console.error(error)
-      res.status(401).send({error, message: 'You are not authorized.'})
+      res.status(401).send({error, message: 'Something went wrong in the authentification process.'})
     }
   }
   
