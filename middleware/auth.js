2caa4fe9752c9c8f356bc6b292aebf79280bcc84
RosaSabater <rosamari_sab@hotmail.com>
2020-11-11 18:36:20 +0100

    fix auth authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,28 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/User');
 
-const auth = async(req,res,next) => {
-    try{
-      const token = req.header('Authorization').replace('Bearer ', '');
-      jwt.verify(token,'mymotherpetsme');
-      const user = await UserModel.findOne({token:token});
-      console.log(user)
-      if(!user){
-        return res.status(401).send({message: 'You got no token.'})
-      }
-      req.user = user;
-      next();
-    } catch (error){
-      console.error(error)
-      res.status(401).send({error, message: 'Something went wrong in the authentification process.'})
-    }
-  }
-  
-  module.exports = auth;
+const auth = async (req, res, next) => {
+	try {
+		const auth = req.header('Authorization');
+
+		if (!auth) {
+			return res.status(401).send({ message: 'You got no authorization header' })
+		}
+
+		const token = auth.replace('Bearer ', '');
+
+		jwt.verify(token, 'mymotherpetsme');
+		const user = await UserModel.findOne({ token: token });
+		console.log(user)
+		if (!user) {
+			return res.status(401).send({ message: 'You got no token.' })
+		}
+		req.user = user;
+		next();
+	} catch (error) {
+		console.error(error)
+		res.status(401).send({ error, message: 'Something went wrong in the authentification process.' })
+	}
+}
+
+module.exports = auth;
