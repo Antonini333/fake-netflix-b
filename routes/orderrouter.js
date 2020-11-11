aa793ba5c42eb787743741bce11fa08bc316d7b4
javigarcia <javigarciaweb@gmail.com>
2020-11-11 20:04:00 +0100

    Update sholl enpoint in orders and cors

--- routes/orderrouter.js
+++ routes/orderrouter.js
@@ -6,7 +6,7 @@ const admin = require('../middleware/admin');
 
 
 router.post('/order/rent', auth, OrderController.rent);
-router.get('/order/show',auth, admin, OrderController.showAll);
+router.get('/order/showAll',auth, admin, OrderController.showAll);
 router.get('/order/show', auth, OrderController.showUser);
 router.delete('/order/:orderId', auth, OrderController.cancelOrder);
 router.put('/order/', auth, OrderController.extendOrder);
