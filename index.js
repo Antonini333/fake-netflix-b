95f466238e9a8b795c5d4acfa46836e7501b9c31
RosaSabater <rosamari_sab@hotmail.com>
2020-11-15 12:07:41 +0100

    more fixes cors

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const cors = require('./middleware/cors');
 
 //Middleware
 app.use(express.json());
-app.use(cors);
+// app.use(cors);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Disposition");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    next();
+});
+app.use(express.json());
 
 
 //db connection
