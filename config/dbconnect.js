6f1d01dfb834bfaaa77a590f22b80aeaeced86f1
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-16 18:12:21 +0100

    Hide user and password from DB connection

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,8 @@ const dbconnect = () => {
 
     //dB connection//////////
     const mongoose = require("mongoose");
-    const uri = process.env.uri || "mongodb+srv://AdminMovies:Abc123@cluster0.x3nrp.mongodb.net/fakeNetflix?retryWrites=true&w=majority";
+    require('dotenv').config();
+    const uri = process.env.uri || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x3nrp.mongodb.net/fakeNetflix?retryWrites=true&w=majority`;
 
     mongoose.connect(uri, {
             useNewUrlParser: true,
